cc74c32ba11ff9176090372334249f03305972c6
nevsther <nevsther@gmail.com>
2014-11-18 16:29:34 +0800

    first test
    just test

---
+++ Assets/Example.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Example : MonoBehaviour {
+    public GameObject[] gos;
+	// Use this for initialization
+	void Start () 
+    {
+        gos = new GameObject[10];
+        for(int i = 0; i < 10; i++)
+        {
+            GameObject go =
+                GameObject.CreatePrimitive(PrimitiveType.Cube);
+            Vector3 v = new Vector3();
+            v.x = Random.Range(-10, 10);
+            v.y = Random.Range(-10, 10);
+            go.transform.position = v;
+            go.name = i.ToString();
+            if(i%2 == 0)
+            {
+                go.AddComponent<ZombieData>();
+            }
+            gos[i] = go;
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+	    foreach(GameObject go in gos)
+        {
+            ZombieData zd = go.GetComponent<ZombieData>();
+            if (zd == null)
+                continue;
+            if (zd.hitPoints > 0)
+                break;
+            print(go.name);
+            zd.hitPoints = 10;
+        }
+	}
+}
---
+++ Assets/Zombie.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Zombie : MonoBehaviour {
+    public float NextTime = 0f;
+    public float Timer = 0.5f;
+    public int Counter = 10;
+    public int minHeight = 1;
+    public int maxHeight = 10;
+    public float HorizontalSpacing = 2f;
+    public float VerticalSpacing = 1f;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+             if (Counter > 0 && Time.fixedTime > NextTime)
+            {
+                NextTime = Time.fixedTime + Timer;
+                for (int j = 10; j > 0; j--)
+                {
+
+                    int randomNumber = Random.Range(minHeight, maxHeight);
+                    for (int i = 0; i < randomNumber; i++)
+                    {
+                        CustomBox cbox = new CustomBox();
+                        cbox.box.transform.position = new Vector3(Counter * HorizontalSpacing, i * VerticalSpacing, j * HorizontalSpacing);
+                        
+                    }
+
+                }
+                Counter--;
+            }
+
+	}
+    class CustomBox { public GameObject box = GameObject.CreatePrimitive(PrimitiveType.Cube);}
+}
